023a39883714edd6993c230a181cecb3db548d7d
taeyoung98 <119taeyoung@naver.com>
2021-01-20 19:03:09 +0900

    map() practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Food({name, image}){
+  return (
+  <>
+  <h1>{name} is delicious!</h1>
+  <img src={image} alt="food"/>
+  </>
+  )
+}
+
+const FoodILike = [
+  {
+    "name": "lamyeon",
+    "src": "https://res.heraldm.com/phpwas/restmb_idxmake.php?idx=507&simg=/content/image/2020/05/23/20200523000093_0.jpg"
+  },
+  {
+    "name": "ram on a skewer",
+    "src": "https://t1.daumcdn.net/cfile/blog/9969FF3E5AE36C2A14"
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {FoodILike.map(dish => <Food name={dish.name} image={dish.src} />)}
     </div>
   );
 }
