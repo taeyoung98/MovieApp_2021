73272c20254e2dbba48a89d369da12ef525013d5
taeyoung98 <119taeyoung@naver.com>
2021-01-21 10:25:04 +0900

    map recap

--- src/App.js
+++ src/App.js
@@ -11,19 +11,26 @@ function Food({name, image}){
 
 const FoodILike = [
   {
+    "id": 1,
     "name": "lamyeon",
     "src": "https://res.heraldm.com/phpwas/restmb_idxmake.php?idx=507&simg=/content/image/2020/05/23/20200523000093_0.jpg"
   },
   {
+    "id": 2,
     "name": "ram on a skewer",
     "src": "https://t1.daumcdn.net/cfile/blog/9969FF3E5AE36C2A14"
   }
 ]
 
+function renderFood(dish){
+  return <Food key={dish.id} name={dish.name} image={dish.src} />;
+}
+
 function App() {
   return (
     <div className="App">
-      {FoodILike.map(dish => <Food name={dish.name} image={dish.src} />)}
+      console.log({FoodILike.map(renderFood)});
+      {FoodILike.map(renderFood)};
     </div>
   );
 }
