d9cb4cd5988b97c45b4344bf5ae9cda6dd61f402
taeyoung98 <119taeyoung@naver.com>
2021-01-27 17:10:49 +0900

    #4.0 Fetching Movies from API with axios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
   state = { // future state is not required. 
     isLoading: true, // just plan
     movies: []
   };
-  componentDidMount(){
-    setTimeout(()=>{ // js. not react
-      this.setState({isLoading: false});
-    }, 6000);
+  getMovies = () => {
+    const movies = axios.get("https:/yts-proxy.now.sh/list_movies.json");
+
+  }
+  // async: "javascript에게 componentDidMount 함수가 끝날 때까지 약간 시간이 걸릴 수 있다"고 전달
+  async componentDidMount(){
+    this.getMovies();
   }
   render(){
     const { isLoading } = this.state;
