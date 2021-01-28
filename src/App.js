bbd78c0534cab84bcd9e2bd425d235de0f61f807
taeyoung98 <119taeyoung@naver.com>
2021-01-28 11:29:05 +0900

    #4.2 Styling the Movies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = { // future state is not required. 
@@ -9,7 +10,7 @@ class App extends React.Component {
   };
   // async(hronous): "javascript에게 getMovies 함수가 끝날 때까지 약간 시간이 걸릴 수 있다"고 전달 == 비동기
   getMovies = async() => {
-    const {data:{data:{movies}}} = await axios.get("https:/yts-proxy.now.sh/list_movies.json");
+    const {data:{data:{movies}}} = await axios.get("https:/yts-proxy.now.sh/list_movies.json?sort_by=rating");
     this.setState({movies, isLoading: false});
   }
   componentDidMount(){
@@ -17,9 +18,22 @@ class App extends React.Component {
   }
   render(){
     const { isLoading, movies } = this.state;
-    return <div>{isLoading ? "Loading" : movies.map(movie => (
-      <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
-    ))}</div>;
+    return (
+      <section className="container">
+      {isLoading ? (
+        <div className="loader">
+          <span className="loader_text">Loading...</span>
+        </div>
+        ) : (
+        <div className="movies">
+          {movies.map(movie => (
+            <Movie key={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
+          ))}
+        </div>
+        )
+      }
+      </section>
+    );
   }
 }
 
--- src/Movie.js
+++ src/Movie.js
@@ -1,9 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
 // component가 state가 필요 없을 경우에는 class component가 될 필요X
-function Movie({id, year, title, summary, poster}){
-    return <h4>{title}</h4>
+function Movie({year, title, summary, poster}){
+    return (
+        <div className="moves__movie">
+            <img src={poster} alt={title} title={title} />
+            <div className="moves__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+
+                
+                <p className="movie__summary">{summary}</p>
+            </div>
+        </div>
+    );
 }
 
 Movie.protoTypes = {
